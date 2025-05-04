bbf7630eb59776253c6e8514a43412ecbe839edd
Drasc001 <dtr2170@email.vccs.edu>
2025-05-04 16:34:37 -0400

    Commit

---
+++ Project4_Patterns/Project4_Patterns/Project4_Patterns.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Pattern A
+    cout << "Pattern A:\n";
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
+
+    // Pattern B
+    cout << "\nPattern B:\n";
+    for (int i = 10; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
